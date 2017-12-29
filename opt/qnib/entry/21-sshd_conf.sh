7ffec59606318a3f9e073cc579870369bcfb059b
Christian Kniep <christian@qnib.org>
2017-12-29 15:36:35 +0100

    ignore hostkey and add SSHD_PORT to sshd_conf

--- opt/qnib/entry/21-sshd_conf.sh
+++ opt/qnib/entry/21-sshd_conf.sh
@@ -3,3 +3,5 @@
 sed -i'' -s 's/#.*StrictHostKeyChecking.*/StrictHostKeyChecking no/' /etc/ssh/ssh_config
 sed -i'' -s 's/#.*PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/ssh_config
 sed -i'' -s "s/#.*Port.*/Port ${SSHD_PORT}/" /etc/ssh/ssh_config
+sed -i'' -s "s/#.*Port.*/Port ${SSHD_PORT}/" /etc/ssh/sshd_config
+sed -i'' -s "s/#.*StrictHostKeyChecking.*/StrictHostKeyChecking no/" /etc/ssh/ssh_config
