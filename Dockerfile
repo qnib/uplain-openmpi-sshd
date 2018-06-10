d0c5c871b4bdc01546a9d4e835ae727cac68acb8
Christian Kniep <christian@qnib.org>
2018-06-10 13:54:04 +0200

    change PS1

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,4 @@ COPY opt/entry/19-cluser-setup.sh \
  opt/entry/20-mkdir.sh \
  opt/entry/21-sshd.sh \
  /opt/entry/
-RUN mkdir -p /chome/cluser/.ssh \
- && chown -R cluser: /chome/cluser \
- && chmod 700 ~cluser/.ssh \
- &&  ssh-keygen -f ~cluser/.ssh/id_rsa -N '' \
- && cp ~cluser/.ssh/id_rsa.pub ~cluser/.ssh/authorized_keys
 ENV SSHD_PORT=22
--- opt/entry/19-cluser-setup.sh
+++ opt/entry/19-cluser-setup.sh
@@ -6,6 +6,6 @@ if [[ ! -d /chome/cluser && ! -d /chome/cluser/.ssh ]];then
   ssh-keygen -f /chome/cluser/.ssh/id_rsa -N ''
   cp /chome/cluser/.ssh/id_rsa.pub /chome/cluser/.ssh/authorized_keys
   echo "mpirun -np 2 --host host1,host2 /usr/local/bin/hello" >> /chome/cluser/.bash_history
+  echo 'export PS1=\u@\h$ ' > /chome/cluser/.bashrc
   chown -R cluser: /chome/cluser
-
 fi
