27ed0b327ac62e937c6b8b8825d6bce4cb083b0e
Christian Kniep <christian@qnib.org>
2018-06-14 12:38:28 -0700

    adjust bash_history

--- opt/entry/19-cluser-setup.sh
+++ opt/entry/19-cluser-setup.sh
@@ -7,8 +7,9 @@ if [[ ! -d /chome/cluser/.ssh ]];then
   chmod 700 /chome/cluser/.ssh
   ssh-keygen -f /chome/cluser/.ssh/id_rsa -N ''
   cp /chome/cluser/.ssh/id_rsa.pub /chome/cluser/.ssh/authorized_keys
-  echo "mpirun -np 2 --host host1,host2 /usr/local/bin/hello" >> /chome/cluser/.bash_history
+  echo "mpirun -np 2 /usr/local/bin/hello 2>/dev/null" >> /chome/cluser/.bash_history
+  echo "mpirun /usr/local/bin/hello 2>/dev/null" >> /chome/cluser/.bash_history
+  echo "salloc -N4 bash" >> /chome/cluser/.bash_history
   cp /opt/qnib/bashrc /chome/cluser/.bashrc
   chown -R cluser: /chome/cluser
-  echo "mpirun -np 2 --host host1,host2 /usr/local/bin/hello" >> /chome/cluser/.bash_history
 fi
